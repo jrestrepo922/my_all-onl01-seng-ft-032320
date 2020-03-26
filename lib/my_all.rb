ead454bee952811db47b20b4912192b4c78c21ff
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 13:37:56 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
 def my_all?(collection)
-
+  i = 0 
+  new_array = []
+  
+  while collection.length > i 
+  new_array << yield(array[i])
+  
+  i = i + 1
+  end 
+  
 end
+
+
+my_all([1, 2, 3]) { |num| 
+
+}
